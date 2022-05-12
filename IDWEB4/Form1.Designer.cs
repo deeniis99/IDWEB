0b6c8d534be4541ab0f597df498ddea6e27169e3
deeniis99 <denasandi@gmail.com>
2022-05-12 22:25:57 +0300

    added files

---
+++ IDWEB4/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿
+namespace IDWEB4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.A = new System.Windows.Forms.TextBox();
+            this.B = new System.Windows.Forms.TextBox();
+            this.C = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.x1r = new System.Windows.Forms.Label();
+            this.x2r = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(101, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(17, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "A";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(101, 108);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(17, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "B";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(101, 167);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(17, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "C";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(342, 299);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(95, 58);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "gaseste radacini";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // A
+            // 
+            this.A.Location = new System.Drawing.Point(154, 59);
+            this.A.Name = "A";
+            this.A.Size = new System.Drawing.Size(100, 22);
+            this.A.TabIndex = 4;
+            // 
+            // B
+            // 
+            this.B.Location = new System.Drawing.Point(154, 108);
+            this.B.Name = "B";
+            this.B.Size = new System.Drawing.Size(100, 22);
+            this.B.TabIndex = 5;
+            // 
+            // C
+            // 
+            this.C.Location = new System.Drawing.Point(154, 162);
+            this.C.Name = "C";
+            this.C.Size = new System.Drawing.Size(100, 22);
+            this.C.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(530, 70);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(22, 17);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "x1";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(530, 167);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(22, 17);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "x2";
+            // 
+            // x1r
+            // 
+            this.x1r.AutoSize = true;
+            this.x1r.Location = new System.Drawing.Point(584, 70);
+            this.x1r.Name = "x1r";
+            this.x1r.Size = new System.Drawing.Size(28, 17);
+            this.x1r.TabIndex = 9;
+            this.x1r.Text = "rez";
+            // 
+            // x2r
+            // 
+            this.x2r.AutoSize = true;
+            this.x2r.Location = new System.Drawing.Point(587, 167);
+            this.x2r.Name = "x2r";
+            this.x2r.Size = new System.Drawing.Size(28, 17);
+            this.x2r.TabIndex = 10;
+            this.x2r.Text = "rez";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.x2r);
+            this.Controls.Add(this.x1r);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.C);
+            this.Controls.Add(this.B);
+            this.Controls.Add(this.A);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox A;
+        private System.Windows.Forms.TextBox B;
+        private System.Windows.Forms.TextBox C;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label x1r;
+        private System.Windows.Forms.Label x2r;
+    }
+}
+
---
+++ IDWEB4/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IDWEB4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double a, b, c;
+            double disc, deno, x1, x2;
+            
+
+            a = Convert.ToDouble(A.Text);
+            b = Convert.ToDouble(B.Text);
+            c = Convert.ToDouble(C.Text);
+            x2 = 0;
+            x1 = 0;
+            if (a == 0)
+            {
+                x1 = -c / b;
+                
+            }
+            else
+            {
+                disc = (b * b) - (4 * a * c);
+                deno = 2 * a;
+                if (disc > 0)
+                {
+                    
+                    x1 = (-b / deno) + (Math.Sqrt(disc) / deno);
+                    x2 = (-b / deno) - (Math.Sqrt(disc) / deno);
+                    
+                }
+                else if (disc == 0)
+                {
+                    
+                    x1 = -b / deno;
+                    
+                }
+                else
+                {
+                    
+                    x1 = -b / deno;
+                    x2 = ((Math.Sqrt((4 * a * c) - (b * b))) / deno);
+                    
+                }
+            }
+            x1r.Text = x1.ToString();
+            x2r.Text = x2.ToString();
+        }
+    }
+}
